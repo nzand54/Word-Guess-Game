9eefb7b45f69ff1ec58da2e0c1a995bf91e78af2
nikki.zand <nzand54@gmail.com>
2019-02-25 12:43:59 -0500

    commit 2

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,8 @@
+
+// 1) randomly genarated word from list of array word options 
+// 2) display amount of spaces per each letter of the randomly selected word option 
+// 3) every time a user makes a guess, log the letter if correct, if incorrect list incorrect letter and minus one from count of guesses left
+// 4)if user hits 0 guesses, post lose Game, click enter to try new game 
+
+// arrayOfOptions = ["Oh Kale Yeah", "Everyday Im brusselin", "I yam what I yam", "Just a little chili", "Romaine Calm", "Let the beet drop", "We make a great pear", "I'm grapeful for you", "One in a melon", "I carrot go on", "Lettuce do our best", "Peas out"]
+// console.log(hello world!)
